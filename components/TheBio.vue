e41def22e4cbf45e568909e61fb2888428bb55f8
Lionel Mann <lionelmann@gmail.com>
2023-06-27 14:45:51 -0400

    edits

--- components/TheBio.vue
+++ components/TheBio.vue
@@ -18,20 +18,26 @@ export default {
 <style lang="css">
 .bio {
   background-color: #f2cf63;
-  padding: 40px;
+  padding: 60px;
 }
 
 .bio {
   column-count: 1;
   column-gap: 50px;
-  font-size: 1.2rem;
+  font-size: 1.3rem;
 }
 
-@media (min-width: 600px) {
-  .bio {
-    column-count: 2;
-  }
+.nuxt-content {
+  max-width: 800px;
+  margin: 0 auto;
 }
+
+/*@media (min-width: 600px) {
+  .bio {
+    max-width: 600px;
+    column-count: 1;
+  }
+}*/
 
 .bio p {
   margin-bottom: 15px;
@@ -43,8 +49,8 @@ export default {
   margin: 0 0.3rem 0 0;
   font-family: "Bebas Neue", cursive;
   font-weight: bold;
-  font-size: 4rem;
+  font-size: 6rem;
   float: left;
-  line-height: 1.05;
+  line-height: 1;
 }
 </style>
---
+++ dist/_nuxt/static/1687891534/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1687891534",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7f51e818"}}}}(null,"\u002F"));
